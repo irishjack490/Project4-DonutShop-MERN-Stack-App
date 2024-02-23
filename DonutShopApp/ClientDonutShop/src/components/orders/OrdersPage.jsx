3151b354977d0c265c5f2434ee591d30b12fd5e5
CosmicGate <jackie.sechrist.3@gmail.com>
2024-02-22 20:12:03 -0600

    Deleting button in progress

--- DonutShopApp/ClientDonutShop/src/components/orders/OrdersPage.jsx
+++ DonutShopApp/ClientDonutShop/src/components/orders/OrdersPage.jsx
@@ -1,32 +1,44 @@
 import React from 'react';
-import { Card, CardText } from 'react-bootstrap'; // Import Card component from react-bootstrap
-//import OrderItem from './OrderItem'; // Component to display each order item
+import { Card, CardText, Button} from 'react-bootstrap'; // Import Card component from react-bootstrap
+
 
 const OrdersPage = ({ orders }) => {
     console.log('Orders:', orders);//this console log shows the structure of the orders array
 
- 
+    if (!orders || (!orders.donuts && !orders.coffees)) {
+      return (
+        <div className="empty-orders">
+          <h2>Your order is empty!</h2>
+          <p>It looks like you haven't added any items to your order yet.</p>
+          <p>Feel free to explore our delicious donuts and coffees and add some to your order.</p>
+        </div>
+      );
+  }
+
+
+  
     return (
       <div>
           <h2>Here is your order</h2>
           <div className="card-container">
               {/* Iterate over the donuts array */}
-              {orders.donuts.map((donut, index) => (
-                  <Card key={`donut-${index}`} className="mb-3">
+              {orders.donuts && orders.donuts.map((donut, index) => (
+                <Card key={`donut-${index}`} className="mb-3">
                     <Card.Body>
                        {donut.name}
-                      ${donut.price}
+                       ${donut.price}
                     </Card.Body>
-                  </Card> 
+                </Card> 
               
               ))}
               {/* Iterate over the coffees array */}
-              {orders.coffees.map((coffee, index) => (
+              {orders.coffees && orders.coffees.map((coffee, index) => (
                   <Card key={`coffee-${index}`} className="mb-3">
                     <Card.Body>
                    {coffee.name}
                    {coffee.size}
                   ${coffee.price}
+                  
                     </Card.Body>
                       {/* <Card.Body>
                           <OrderItem order={coffee} orders={orders}/>
